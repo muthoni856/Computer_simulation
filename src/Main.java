35947270e015b2a86a69f930c697423e3381889d
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2024-05-17 11:53:50 +0300

    Discrete Event Priority Queue

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+public class DiscreteEventSimulation {
+    public static void main(String[] args) {
+        Queue<Event> eventQueue = new PriorityQueue<>(); //Priority queue for managing events
+        double currentTime = 0; //Current simulation time
+        int queueLength = 0; // Length of the queue
+        boolean serverBusy = false; // Flag to indicate if the server is busy
+        double serviceTimeMean = 5.0;
+        double simulationTime = 100;// Total simulation time
+
+        // Add initial events
+        eventQueue.add(new Event(currentTime, "CustomerArrival"));
+
+        while (currentTime < simulationTime) {
+            Event event = eventQueue.poll(); // .poll()- Retrieves and removes the head of this queue, or returns null if this queue is empty
+            currentTime = event.getTime();
+
+            if (event.getType().equals("CustomerArrival")) {
+                //Handling customer arrival event
+                System.out.println("Customer arrived at time " + currentTime);
+                queueLength++;
+                if (!serverBusy) {
+                    // Server is idle, start service
+                    serverBusy = true;
+                    double serviceTime = serviceTimeMean - Math.log(Math.random()) * serviceTimeMean;
